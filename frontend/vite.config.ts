1036c715d877b461beaba554e86db9a04caff471
David Le <david29101000@gmail.com>
2024-12-15 16:54:57 +0100

    added global config ip

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to all IPs
+    port: 5173       // Ensure the port matches your current setup
+  }
 })
 
